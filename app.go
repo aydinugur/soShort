681579a1cabca6e08e2261036452edf7cffd28b3
uaydin <aydinugur@yahoo.com>
2019-05-23 23:15:56 +0300

    Boom 💥 a REST api

--- app.go
+++ app.go
@@ -1,5 +1,12 @@
 package main
 
+import "net/http"
+
+func sayHello(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Hello world!"))
+}
+
 func main() {
-	
+	http.HandleFunc("/", sayHello)
+	http.ListenAndServe(":8080", nil)
 }
